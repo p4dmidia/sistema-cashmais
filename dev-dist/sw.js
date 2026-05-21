f5736a59c2b4799f7ba7d14b99054f5379893e10
Dr Agrotec <agenciap4d@gmail.com>
2026-05-21 11:16:44 -0300

    versão atualizada caixa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2eq3h59qh28"
+    "revision": "0.ehm8oe2msqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
