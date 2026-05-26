d371adf24568407baf2ad99ae9c21cb81e93fee4
P4D Midia <agenciap4d@gmail.com>
2026-05-26 13:36:18 -0300

    atualização refresh

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0tt3rvm51b8"
+    "revision": "0.0u21q05hq3s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
